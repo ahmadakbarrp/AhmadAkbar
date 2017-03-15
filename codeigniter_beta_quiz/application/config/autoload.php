9bfe1a9d829112858eac427da0aa33436a871faf
ahmadakbarrp <ahmadakbarrifaldiputra5@gmail.com>
2017-03-15 15:21:44 +0700

    Projeck Kuis
    Kuis1

--- codeigniter_beta_quiz/application/config/autoload.php
+++ codeigniter_beta_quiz/application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('database','datatables');
 
 /*
 | -------------------------------------------------------------------
@@ -90,7 +90,6 @@ $autoload['drivers'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 $autoload['helper'] = array('url');
-
 /*
 | -------------------------------------------------------------------
 |  Auto-load Config files
---
+++ codeigniter_beta_quiz/application/controllers/Perusahaan.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Perusahaan extends CI_Controller {
+
+	public function index()
+	{
+			{
+				$this->load->model('Perusahaan_Model');
+				$data['perusahaan_array']=$this->Perusahaan_Model->getDataPerusahaan();
+				$this->load->view('perusahaan',$data);
+			}
+		
+	}
+
+}
+
+/* End of file Pegawai.php */
+/* Location: ./application/controllers/Pegawai.php */
+
+ ?>
